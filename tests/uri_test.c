149d344e143483bbb5be487fa78095a1b75e858f
Eric Rubio <penmanglewood@fastmail.fm>
2014-01-30 15:47:39 +0800

    Add URI consecutive slash tests

--- tests/uri_test.c
+++ tests/uri_test.c
@@ -55,6 +55,71 @@ char *test_relative_path()
     return NULL;
 }
 
+char *test_path()
+{
+    char *test_str = "/";
+    bstring expected = bfromcstr("/");
+    bstring res;
+
+    res = uri_normalize(test_str);
+
+    mu_assert(biseq(expected, res) == 1, "Slash should stay the same");
+
+    return NULL;
+}
+
+char *test_consecutive_slashes_1()
+{
+    char *test_str = "//";
+    bstring expected = bfromcstr("/");
+    bstring res;
+
+    res = uri_normalize(test_str);
+
+    mu_assert(biseq(expected, res) == 1, "Consecutive slashes should be reduced to one (1)");
+
+    return NULL;
+}
+
+char *test_consecutive_slashes_2()
+{
+    char *test_str = "/a//";
+    bstring expected = bfromcstr("/a/");
+    bstring res;
+
+    res = uri_normalize(test_str);
+
+    mu_assert(biseq(expected, res) == 1, "Consecutive slashes should be reduced to one (2)");
+
+    return NULL;
+}
+
+char *test_consecutive_slashes_3()
+{
+    char *test_str = "/a////";
+    bstring expected = bfromcstr("/a/");
+    bstring res;
+
+    res = uri_normalize(test_str);
+
+    mu_assert(biseq(expected, res) == 1, "Consecutive slashes should be reduced to one (3)");
+
+    return NULL;
+}
+
+char *test_consecutive_slashes_4()
+{
+    char *test_str = "////a";
+    bstring expected = bfromcstr("/a");
+    bstring res;
+
+    res = uri_normalize(test_str);
+
+    mu_assert(biseq(expected, res) == 1, "Consecutive slashes should be reduced to one (4)");
+
+    return NULL;
+}
+
 char *all_tests() {
     mu_suite_start();
 
@@ -62,6 +127,11 @@ char *all_tests() {
     mu_run_test(test_url);
     mu_run_test(test_nonpath);
     mu_run_test(test_relative_path);
+    mu_run_test(test_path);
+    mu_run_test(test_consecutive_slashes_1);
+    mu_run_test(test_consecutive_slashes_2);
+    mu_run_test(test_consecutive_slashes_3);
+    mu_run_test(test_consecutive_slashes_4);
 
     return NULL;
 }
